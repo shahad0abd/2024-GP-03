81c294a181793609b0c92b3aafac4add9e9a267a
shahad0abd <sh1.abullah@gmail.com>
2024-03-10 17:51:44 +0300

    first commit

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,105 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Stopwatch stopwatch;
+  late Timer t;
+
+  void startTimer() {
+    stopwatch.start();
+  }
+
+  void stopTimer() {
+    stopwatch.stop();
+  }
+
+  void resetTimer() {
+    stopwatch.reset();
+  }
+
+  void stopAndResetTimer() {
+    stopTimer();
+    resetTimer();
+  }
+
+  String returnFormattedText() {
+    var milli = stopwatch.elapsed.inMilliseconds;
+    String milliseconds = (milli % 1000)
+        .toString()
+        .padLeft(2, "0"); // 1001 % 1000 = 1, 1450 % 1000 = 450
+    String seconds = ((milli ~/ 1000) % 60).toString().padLeft(2, "0");
+    String minutes = ((milli ~/ 1000) ~/ 60).toString().padLeft(2, "0");
+    String hours =
+        ((milli ~/ (1000 * 60 * 60)) % 24).toString().padLeft(2, "0");
+    return "$hours:$minutes:$seconds";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    stopwatch = Stopwatch();
+    t = Timer.periodic(Duration(microseconds: 30), (timer) {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Color.fromARGB(255, 2, 129, 55),
+                      width: 4,
+                    ),
+                  ),
+                  child: Text(
+                    returnFormattedText(),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CupertinoButton(
+                    onPressed: startTimer,
+                    child: Text("ON"),
+                  ),
+                  SizedBox(width: 15),
+                  CupertinoButton(
+                    onPressed: stopAndResetTimer,
+                    child: Text("OFF"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
